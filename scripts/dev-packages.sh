08e3d11b488bb15e98e9a8e283858fcc41ae77b8
Joshua Warren <josh@creatuity.com>
2016-03-01 18:05:54 -0600

    Adding PHP 5.6 and a test environment

--- scripts/dev-packages.sh
+++ scripts/dev-packages.sh
@@ -9,3 +9,8 @@ pip install ifs
 wget -q https://github.com/cbednarski/hostess/releases/download/v0.1.0/hostess_linux_amd64
 chmod +x hostess_linux_amd64
 mv hostess_linux_amd64 /usr/local/bin/hostess
+
+apt-get install -y python-software-properties software-properties-common
+add-apt-repository -y ppa:ondrej/php5-5.6
+apt-get update
+apt-get install -y php5
